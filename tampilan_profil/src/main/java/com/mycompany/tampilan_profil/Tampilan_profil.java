89eadd3c9e83dcffca0d963729165a6c06830561
abdullahrobith <145589841+abdullahrobith@users.noreply.github.com>
2024-10-06 23:24:46 +0700

    Add files via upload

---
+++ tampilan_profil/src/main/java/com/mycompany/tampilan_profil/Tampilan_profil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tampilan_profil;
+
+/**
+ *
+ * @author USER
+ */
+public class Tampilan_profil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
